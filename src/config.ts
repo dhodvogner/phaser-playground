e03d58f974f011822611d357ff299d68ba2c25e5
Daniel Hodvogner <dhodvogner@gmail.com>
2019-05-13 18:17:54 +0200

    game scene added

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,12 @@
+import { GameScene } from "./game.scene";
+
 export const config = {
   title: "Phaser playground",
   width: 640,
   height: 480,
   parent: "game",
   backgroundColor: "#18216D",
+  scene: [
+    GameScene,
+  ],
 };
---
+++ src/game.scene.ts
@@ -0,0 +1,24 @@
+import { Scene } from "phaser";
+
+export class GameScene extends Scene {
+  constructor() {
+    super({
+      key: 'GameScene',
+    });
+  }
+
+  public init(params): void {
+    console.log('Yeey GameScene!');
+    // TODO
+  }
+  public preload(): void {
+    // TODO
+  }
+
+  public create(): void {
+    // TODO
+  }
+  public update(time): void {
+    // TODO
+  }
+}
